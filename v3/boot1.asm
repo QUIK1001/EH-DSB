08f72fc626e4b8e1787f279fadcf51880f39dc3d
quik <belov0507artem@gmail.com>
2026-03-15 21:28:06 +0300

    Update boot1.asm

--- v3/boot1.asm
+++ v3/boot1.asm
@@ -3,42 +3,47 @@ org 0x7E00
 
 start:
     mov [boot_drive], dl
-    
+
     mov si, msg_loading
     call print_string
-    
+
     mov ax, 0x1000
     mov es, ax
-    mov bx, 0x0000
-    
+    xor bx, bx
+
     mov ah, 0x02
     mov al, 128
-    mov ch, 0x00
-    mov cl, 0x0C
-    mov dh, 0x00
+    mov ch, 0
+    mov cl, 12
+    mov dh, 0
     mov dl, [boot_drive]
     int 0x13
     jc disk_error
-    
+
     cmp al, 128
     jne disk_error
-    
+
     mov si, msg_success
     call print_string
-    
+
+    call delay_1sec
+
     mov si, ascii_art
     call print_string
-    
+
+    mov si, license_text
+    call print_string
+
     call delay_2sec
-    
+
     cli
-    
+
     lgdt [gdt_descriptor]
-    
+
     mov eax, cr0
     or eax, 1
     mov cr0, eax
-    
+
     jmp 0x08:protected_mode
 
 disk_error:
@@ -55,6 +60,19 @@ print_string:
     int 0x10
     jmp .loop
 .done:
+    ret
+
+delay_1sec:
+    pusha
+    mov ah, 0x00
+    int 0x1A
+    mov bx, dx
+    add bx, 36
+.wait:
+    int 0x1A
+    cmp dx, bx
+    jl .wait
+    popa
     ret
 
 delay_2sec:
@@ -78,11 +96,11 @@ protected_mode:
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    
+
     mov esp, 0x90000
-    
+
     jmp 0x08:0x10000
-    
+
 gdt_start:
     dq 0x0000000000000000
 
@@ -90,16 +108,16 @@ gdt_code:
     dw 0xFFFF
     dw 0x0000
     db 0x00
-    db 0x9A
-    db 0xCF
+    db 10011010b
+    db 11001111b
     db 0x00
 
 gdt_data:
     dw 0xFFFF
     dw 0x0000
     db 0x00
-    db 0x92
-    db 0xCF
+    db 10010010b
+    db 11001111b
     db 0x00
 
 gdt_end:
@@ -118,5 +136,27 @@ ascii_art db 0x0D, 0x0A
           db '| __| || |  |   \/ __|| _ )  ', 0x0D, 0x0A
           db '| _|| __ |  | |) \__ \| _ \  ', 0x0D, 0x0A
           db '|___|_||_|  |___/|___/|___/ ', 0x0D, 0x0A, 0
+
+license_text db 0x0D, 0x0A
+             db 'This is free and unencumbered software released into the public domain.', 0x0D, 0x0A
+             db 'Anyone is free to copy, modify, publish, use, compile, sell, or', 0x0D, 0x0A
+             db 'distribute this software, either in source code form or as a compiled', 0x0D, 0x0A
+             db 'binary, for any purpose, commercial or non-commercial, and by any', 0x0D, 0x0A
+             db 'means.', 0x0D, 0x0A
+             db 'In jurisdictions that recognize copyright laws, the author or authors', 0x0D, 0x0A
+             db 'of this software dedicate any and all copyright interest in the', 0x0D, 0x0A
+             db 'software to the public domain. We make this dedication for the benefit', 0x0D, 0x0A
+             db 'of the public at large and to the detriment of our heirs and', 0x0D, 0x0A
+             db 'successors. We intend this dedication to be an overt act of', 0x0D, 0x0A
+             db 'relinquishment in perpetuity of all present and future rights to this', 0x0D, 0x0A
+             db 'software under copyright law.', 0x0D, 0x0A
+             db 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,', 0x0D, 0x0A
+             db 'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF', 0x0D, 0x0A
+             db 'MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.', 0x0D, 0x0A
+             db 'IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR', 0x0D, 0x0A
+             db 'OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,', 0x0D, 0x0A
+             db 'ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR', 0x0D, 0x0A
+             db 'OTHER DEALINGS IN THE SOFTWARE.', 0x0D, 0x0A
+             db 'For more information, please refer to <https://unlicense.org>', 0x0D, 0x0A, 0
 
 times 5120-($-$$) db 0
